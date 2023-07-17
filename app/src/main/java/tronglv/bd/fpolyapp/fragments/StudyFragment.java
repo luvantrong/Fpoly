82de75c930ae8cc25569227084456204368c6d27
luvantrong <102342647+luvantrong@users.noreply.github.com>
2023-07-17 16:54:35 +0700

    Screen Study _v1

--- app/src/main/java/tronglv/bd/fpolyapp/fragments/StudyFragment.java
+++ app/src/main/java/tronglv/bd/fpolyapp/fragments/StudyFragment.java
@@ -2,7 +2,10 @@ package tronglv.bd.fpolyapp.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,57 +13,44 @@ import android.view.ViewGroup;
 
 import tronglv.bd.fpolyapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StudyFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class StudyFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public StudyFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StudyFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static StudyFragment newInstance(String param1, String param2) {
+    //Truyền data vào fragment khi khởi tạo
+    public static StudyFragment newInstance() {
         StudyFragment fragment = new StudyFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+//        args.putSerializable("notifications", listNotification);
+//        fragment.setArguments(args);
         return fragment;
     }
 
+    //Đọc data
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+//            listNotification = (ArrayList<Notification>) getArguments().getSerializable("notifications");
         }
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_study, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_study, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+//        rvListProducts = view.findViewById(R.id.rvListProducts);
+//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+//        rvListProducts.setLayoutManager(layoutManager);
+//        AdapterProdutcs adapter = new AdapterProdutcs(getContext(), listProducts);
+//        rvListProducts.setAdapter(adapter);
     }
 }

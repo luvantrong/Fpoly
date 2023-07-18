2d07c2326e753b764782bb1c946669cfe228a720
PLong2003 <99625263+PLong2003@users.noreply.github.com>
2023-07-18 22:18:33 +0700

    btnStarter

--- app/src/main/java/tronglv/bd/fpolyapp/views/StarterDemoActivity.java
+++ app/src/main/java/tronglv/bd/fpolyapp/views/StarterDemoActivity.java
@@ -2,15 +2,31 @@ package tronglv.bd.fpolyapp.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import tronglv.bd.fpolyapp.R;
 
 public class StarterDemoActivity extends AppCompatActivity {
 
+    LinearLayout btnLoginStarter ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starter_demo);
+
+        btnLoginStarter = findViewById(R.id.btnLoginStarter);
+
+        btnLoginStarter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(StarterDemoActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 }

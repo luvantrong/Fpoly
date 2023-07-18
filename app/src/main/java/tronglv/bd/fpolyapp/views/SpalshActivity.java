df8f4202d0947011306dd05571aa67b2d1ee1042
luvantrong <102342647+luvantrong@users.noreply.github.com>
2023-07-18 23:40:08 +0700

    Cpmplete Start demo

--- app/src/main/java/tronglv/bd/fpolyapp/views/SpalshActivity.java
+++ app/src/main/java/tronglv/bd/fpolyapp/views/SpalshActivity.java
@@ -2,7 +2,9 @@ package tronglv.bd.fpolyapp.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import tronglv.bd.fpolyapp.R;
 
@@ -12,5 +14,14 @@ public class SpalshActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spalsh);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), StarterDemoActivity.class));
+                overridePendingTransition(R.anim.anim_enter_splash, R.anim.anim_exit_splash);
+                finish();
+            }
+        }, 3000);
     }
 }

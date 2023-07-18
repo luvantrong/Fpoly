5e0be5370e0b0a9ed3702c7bed1e7754ec2a02ee
PLong2003 <99625263+PLong2003@users.noreply.github.com>
2023-07-18 15:44:32 +0700

    Schedule Fragment

---
+++ app/src/main/java/tronglv/bd/fpolyapp/viewHolders/ScheduleViewHolder.java
@@ -0,0 +1,43 @@
+package tronglv.bd.fpolyapp.viewHolders;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import tronglv.bd.fpolyapp.R;
+import tronglv.bd.fpolyapp.interfaces.ItemClickListener;
+
+public class ScheduleViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener  {
+
+    public TextView txtDay, txtRoom, txtNameCourse;
+    private ItemClickListener itemClickListener;
+
+    public ScheduleViewHolder(@NonNull View itemView) {
+        super(itemView);
+        itemView.setOnClickListener(this);
+        itemView.setOnClickListener(this);
+        itemView.setOnLongClickListener(this);
+        txtDay = itemView.findViewById(R.id.txtDay);
+        txtRoom = itemView.findViewById(R.id.txtRoom);
+        txtNameCourse = itemView.findViewById(R.id.txtNameCourse);
+    }
+
+
+    public void setItemClickListener(ItemClickListener itemClickListener)
+    {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        itemClickListener.onClick(v,getAdapterPosition(),false);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        itemClickListener.onClick(v,getAdapterPosition(),true);
+        return true;
+    }
+}
